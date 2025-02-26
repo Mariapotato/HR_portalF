c7fcb44c2377f41b50e4a5fc3d03979a751fb11e
SANE4KACODE <105416716+Mariapotato@users.noreply.github.com>
2025-02-26 19:52:11 +0300

    Портал для кадров
    HR Porta

---
+++ frontend1/src/components/Footer.jsx
@@ -0,0 +1,4 @@
+export default function Footer() {
+    const currentYear = new Date().getFullYear();
+    return <footer className="footer-block">ТРВП-011. Андреева Ольга РК6-71Б. ©{currentYear} Все парава защищены.</footer>;
+}
